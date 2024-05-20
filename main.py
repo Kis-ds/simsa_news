1654227079061c2b9f004160a904ef5bb358b6d2
Kis-ds <128333406+Kis-ds@users.noreply.github.com>
2024-05-20 10:32:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ df['date_string'] = df['date_string'].astype(str)
 df['total_score'] = df['total_score'].astype(float)
 
 
-df_plot = df[['date_string', '대분류', '중분류', '그룹명', '업체명', 'title', 'summary', 'link']].copy()
+df_plot = df[['date_string', '대분류', '중분류', '업체명', 'title', 'summary', 'link']].copy()
 df_plot.columns = ['날짜', '산업', '분야','기업명', '기사제목', '본문요약', '링크']
 
 TODAY_VAR = df.date_string.max()
